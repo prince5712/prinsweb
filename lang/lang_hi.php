6f037b7e3a10e56043b456481f74b3a6dba7463e
Prince Jatav <prinsberwa@gmail.com>
2025-07-29 20:11:48 +0530

    Add lang_hi.php

---
+++ lang/lang_hi.php
@@ -0,0 +1,21 @@
+<?php
+// lang/lang_hi.php
+
+define('LANG_CODE', 'hi');
+define('LANG_NAME', 'हिंदी');
+define('TXT_HOME', 'होम');
+define('TXT_ABOUT', 'बारे में');
+define('TXT_CONTACT', 'संपर्क');
+define('TXT_WELCOME', 'स्वागत है');
+define('TXT_SPLASH_MESSAGE', 'लोड हो रहा है...');
+define('TXT_OFFLINE_MESSAGE', 'कोई इंटरनेट कनेक्शन नहीं');
+define('TXT_THEME_AUTO', 'ऑटो');
+define('TXT_THEME_LIGHT', 'लाइट');
+define('TXT_THEME_DARK', 'डार्क');
+define('TXT_SITE_DESCRIPTION', 'यह PHP, Bootstrap 5 और आधुनिक सुविधाओं के साथ निर्मित एक नमूना वेबसाइट है।');
+define('TXT_ABOUT_CONTENT', 'यह बारे में पृष्ठ है। हमारे बारे में अधिक जानें।');
+define('TXT_CONTACT_CONTENT', 'यह संपर्क पृष्ठ है। हमसे संपर्क करें।');
+define('TXT_LANGUAGE', 'भाषा');
+define('TXT_THEME', 'थीम');
+
+?>
